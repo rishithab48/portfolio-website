3b2f76b50b5bf7d0c1731dfe49f159c06d1b200d
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2024-09-22 00:41:56 -0400

    Creating Portfolio app.

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-12 lg:gap-12">
+        <div className="col-span-7 flex flex-col justify-center text-center sm:text-left lg:text-left">
+          <h1 className="text-white text-left mb-4 text-4xl lg:text-6xl sm:text-5xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
+            <br />
+            {/* Rishitha Bura */}
+            <span>
+            <TypeAnimation
+              sequence={[
+                "Rishitha",
+                2500,
+                "Full-Stack Developer",
+                2500,
+                "Web Developer",
+                2500,
+                "Data Analyst",
+                2500,
+              ]}
+              wrapper="span"
+              speed={10}
+              repeat={Infinity}
+            />
+            </span>
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+            I am a full stack developer!
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download Resume
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-16">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero-image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
