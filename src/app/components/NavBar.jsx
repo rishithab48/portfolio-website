e62b2c32616a57fa231ebe2b4b36ebca81a5177b
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2024-09-28 01:55:38 -0400

    Basic Mobile Menu built

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,41 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import MenuOverlay from "./MenuOverlay";
 
 const navlinks = [
-    {
-        title:"About",
-        path:"#about",
-    },
-    {
-        title:"Projects",
-        path:"#projects",
-    },
-    {
-        title:"Contacts",
-        path:"#contacts",
-    },
-
-]
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contacts",
+    path: "#contacts",
+  },
+];
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto pt-4 px-5">
-        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           LOGO
         </Link>
-        <div className="menu md:w-auto" id="navbar">
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 rounded border border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <FaBars className="h-5 w-5" />
+            </button>
+          ) : (
+            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 rounded border border-slate-200 text-slate-200 hover:text-white hover:border-white">
+                <IoClose className="h-5 w-5"/>
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {
-                navlinks.map((link,index)=>(
-                    <li key={index}>
-                        <NavLink href={link.path} title={link.title}/>
-                    </li>
-                ))
-            }
+            {navlinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navlinks}/>: null}
     </nav>
   );
 };
